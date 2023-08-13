7c8fdec0997f7c449afbc6176ae6789a52ecaa7e
Maite <martinezf.maite@gmail.com>
2023-08-13 18:43:14 -0300

    Crea el proyecto utilizando Maven

--- README.md
+++
@@ -1,1 +0,0 @@
-# reposhi
---
+++ src/main/java/logica/Socio.java
@@ -0,0 +1,26 @@
+package logica;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Socio extends Usuario {
+
+	private ArrayList<Registro> arrayRegistro;	
+	
+    public Socio(String nickname, String nombre, String apellido, String correoElectronico, Date fechaNacimiento, ArrayList<Registro> arrayRegistro)
+    {
+    	super(nickname, nombre, apellido, correoElectronico, fechaNacimiento);
+    	this.arrayRegistro= arrayRegistro;
+    }
+
+	public ArrayList<Registro> getArrayRegistro() {
+		return arrayRegistro;
+	}
+
+	public void setArrayRegistro(ArrayList<Registro> arrayRegistro) {
+		this.arrayRegistro = arrayRegistro;
+	}
+
+
+
+}

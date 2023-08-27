600743f5189d113c9bc14648fe8fe96f0c430609
Maite <martinezf.maite@gmail.com>
2023-08-27 13:11:23 -0300

    GUI Modificar usuario

--- tarea1/src/main/java/presentacionConsultas/GUIConsultaUsuario.java
+++ tarea1/src/main/java/presentacionConsultas/GUIConsultaUsuario.java
@@ -26,6 +26,11 @@ import javax.swing.JScrollPane;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Vector;
@@ -36,8 +41,48 @@ import javax.swing.JComboBox;
 public class GUIConsultaUsuario extends JInternalFrame {
 	private JTable tablaClases;
 	private JTable tablaRegistro;
+	JComboBox cmbUsuarios = new JComboBox();
+	List<Usuario> usuarios = new ArrayList<>();
+	JLabel labelNickname = new JLabel("Nickname:");
+	JLabel labelNombre = new JLabel("Nombre:");
+	JLabel labelApellido = new JLabel("Apellido:");
+	JLabel labelEmail = new JLabel("Email: ");
+	JLabel labelFechaNac = new JLabel("Fecha Nacimiento:");
+	JLabel labelDescripcion = new JLabel("Descripcion:");
+	JLabel labelBiografia = new JLabel("Biografia:");
+	JLabel labelSitioWeb = new JLabel("Sitio Web:");
+	JLabel labelClases = new JLabel("Clases:");
+	JLabel nickname = new JLabel("");
+	JLabel nombre = new JLabel("");
+	JLabel apellido = new JLabel("");
+	JLabel email = new JLabel("");
+	JLabel fechaNac = new JLabel("");
+	JLabel descripcion = new JLabel("");
+	JLabel biografia = new JLabel("");
+	JLabel sitioWeb = new JLabel("");
+	JLabel labelRegistros = new JLabel("Registros:");
+	JScrollPane scrollPaneRegistros = new JScrollPane();
+	
+	private void setupActions(IUsuario iUsuario) {
+		addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentShown(ComponentEvent e) {
+				cmbUsuarios.removeAllItems();
+				List<Usuario> usuariosNuevos = new ArrayList<>();
+				usuariosNuevos = iUsuario.getUsuarios();
+				usuarios = usuariosNuevos;
+					
+				for(Usuario u: usuarios) {
+					cmbUsuarios.addItem(u.getNickname());
+				}
+				
+				cmbUsuarios.setSelectedIndex(-1);	
+			}
+		});
+	}
 
 	public GUIConsultaUsuario(IUsuario iUsuario) {
+		setupActions(iUsuario);
 		setClosable(true);
 		setTitle("Consultar usuario");
 		setBounds(100, 100, 559, 500);
@@ -88,179 +133,167 @@ public class GUIConsultaUsuario extends JInternalFrame {
 		iClase.altaDictadoClase("c4", a4, new Date(), profe2.getNickname(), "12:00", "url", new Date());
 		
 		DefaultListModel<String> lista = new DefaultListModel<String>();
-		
-		JComboBox cmbUsuarios = new JComboBox();
+
 
 		cmbUsuarios.setBounds(141, 32, 176, 21);
-		List<Usuario> usuarios = iUsuario.getUsuarios();
-		try {
-			for(Usuario u: usuarios) {
-				cmbUsuarios.addItem(u.getNickname());
-			}
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Error al cargar los usuarios", "Error", JOptionPane.ERROR_MESSAGE);
-		}
-		cmbUsuarios.setSelectedIndex(-1);
 		getContentPane().add(cmbUsuarios);
 		
-		JLabel labelNickname = new JLabel("Nickname:");
 		labelNickname.setBounds(31, 78, 105, 13);
 		getContentPane().add(labelNickname);
 		
-		JLabel labelNombre = new JLabel("Nombre:");
 		labelNombre.setBounds(31, 101, 105, 13);
 		getContentPane().add(labelNombre);
 		
-		JLabel labelApellido = new JLabel("Apellido:");
 		labelApellido.setBounds(31, 124, 105, 13);
 		getContentPane().add(labelApellido);
 		
-		JLabel labelEmail = new JLabel("Email: ");
 		labelEmail.setBounds(31, 147, 105, 13);
 		getContentPane().add(labelEmail);
 		
-		JLabel labelFechaNac = new JLabel("Fecha Nacimiento:");
 		labelFechaNac.setBounds(31, 170, 105, 15);
 		getContentPane().add(labelFechaNac);
 		
-		JLabel labelDescripcion = new JLabel("Descripcion:");
 		labelDescripcion.setBounds(31, 195, 105, 13);
 		labelDescripcion.setVisible(false);
 		getContentPane().add(labelDescripcion);
 		
-		JLabel labelBiografia = new JLabel("Biografia:");
 		labelBiografia.setBounds(31, 218, 105, 13);
 		labelBiografia.setVisible(false);
 		getContentPane().add(labelBiografia);
 		
-		JLabel labelSitioWeb = new JLabel("Sitio Web:");
 		labelSitioWeb.setBounds(31, 241, 105, 13);
 		labelSitioWeb.setVisible(false);
 		getContentPane().add(labelSitioWeb);
 		
-		JLabel labelClases = new JLabel("Clases:");
 		labelClases.setBounds(31, 264, 105, 13);
 		labelClases.setVisible(false);
 		getContentPane().add(labelClases);
 		
-		JLabel nickname = new JLabel("");
 		nickname.setBounds(147, 78, 308, 13);
 		getContentPane().add(nickname);
 		
-		JLabel nombre = new JLabel("");
 		nombre.setBounds(147, 101, 308, 13);
 		getContentPane().add(nombre);
 		
-		JLabel apellido = new JLabel("");
 		apellido.setBounds(147, 124, 308, 13);
 		getContentPane().add(apellido);
 		
-		JLabel email = new JLabel("");
 		email.setBounds(147, 147, 308, 13);
 		getContentPane().add(email);
 		
-		JLabel fechaNac = new JLabel("");
 		fechaNac.setBounds(147, 171, 308, 13);
 		getContentPane().add(fechaNac);
 		
-		JLabel descripcion = new JLabel("");
 		descripcion.setBounds(147, 195, 308, 13);
 		getContentPane().add(descripcion);
 		
-		JLabel biografia = new JLabel("");
 		biografia.setBounds(147, 218, 308, 13);
 		getContentPane().add(biografia);
 		
-		JLabel sitioWeb = new JLabel("");
 		sitioWeb.setBounds(147, 241, 308, 13);
 		getContentPane().add(sitioWeb);
 		
-		JLabel labelRegistros = new JLabel("Registros:");
 		labelRegistros.setBounds(31, 195, 73, 13);
 		labelRegistros.setVisible(false);
 		getContentPane().add(labelRegistros);
 		
+		tablaRegistro = new JTable();
+		tablaClases = new JTable();
 		DefaultTableModel modelRegistro = new DefaultTableModel();
+		DefaultTableModel modelClases = new DefaultTableModel();
+		
 		modelRegistro.addColumn("Clase");
 		modelRegistro.addColumn("Fecha de Registro");
 
-		DefaultTableModel modelClases = new DefaultTableModel();
+		JScrollPane scrollPaneClases = new JScrollPane();
 		modelClases.addColumn("Nombre");
 		modelClases.addColumn("Fecha");
 		modelClases.addColumn("Hora de Inicio");
-		
-		JScrollPane scrollPaneClases = new JScrollPane();
 		scrollPaneClases.setBounds(31, 287, 491, 174);
 		getContentPane().add(scrollPaneClases);
-		
-		tablaClases = new JTable();
+	
 		scrollPaneClases.setViewportView(tablaClases);
 		scrollPaneClases.setVisible(false);
 		tablaClases.setVisible(false);
 		tablaClases.setModel(modelClases);
 		
-		JScrollPane scrollPaneRegistros = new JScrollPane();
 		scrollPaneRegistros.setBounds(29, 218, 493, 243);
 		getContentPane().add(scrollPaneRegistros);
 		
-		tablaRegistro = new JTable();
 		scrollPaneRegistros.setViewportView(tablaRegistro);
 		tablaRegistro.setVisible(false);
 		scrollPaneRegistros.setVisible(false);
 		tablaRegistro.setModel(modelRegistro);
 		
-		cmbUsuarios.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		cmbUsuarios.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
 				String nombreUsuario = (String) cmbUsuarios.getSelectedItem();
 				Usuario usuario = iUsuario.buscarUsuario(nombreUsuario);
-				nickname.setText(usuario.getNickname());
-				nombre.setText(usuario.getNombre());
-				apellido.setText(usuario.getApellido());
-				email.setText(usuario.getCorreoElectronico());
-				fechaNac.setText(usuario.getFechaNacimiento().toString());
-				
-				if(usuario instanceof Socio) {
-					Socio socio = (Socio) usuario;
-					labelRegistros.setVisible(true);
+				if(usuario != null) {
+					nickname.setText(usuario.getNickname());
+					nombre.setText(usuario.getNombre());
+					apellido.setText(usuario.getApellido());
+					email.setText(usuario.getCorreoElectronico());
+					fechaNac.setText(usuario.getFechaNacimiento().toString());
+					
+					if(usuario instanceof Socio) {
+						Socio socio = (Socio) usuario;
+						labelRegistros.setVisible(true);
+						labelDescripcion.setVisible(false);
+						labelBiografia.setVisible(false);
+						labelSitioWeb.setVisible(false);
+						labelClases.setVisible(false);
+						tablaClases.setVisible(false);
+						scrollPaneClases.setVisible(false);
+						descripcion.setText("");
+						biografia.setText("");
+						sitioWeb.setText("");
+
+						modelRegistro.setRowCount(0);
+						List<Registro> registros = socio.getArrayRegistro();
+						for (Registro r: registros) {
+							modelRegistro.addRow(new Object[] {r.getclase().getNombreClase(), r.getfechaRegistro().toString()});
+						}
+
+						tablaRegistro.setVisible(true);
+						scrollPaneRegistros.setVisible(true);
+					} else {
+						Profesor profesor = (Profesor) usuario;
+						labelRegistros.setVisible(false);
+						labelDescripcion.setVisible(true);
+						labelBiografia.setVisible(true);
+						labelSitioWeb.setVisible(true);
+						labelClases.setVisible(true);
+						tablaRegistro.setVisible(false);
+						scrollPaneRegistros.setVisible(false);
+						descripcion.setText(profesor.getDescripcionGeneral());
+						biografia.setText(profesor.getBiografia());
+						sitioWeb.setText(profesor.getSitioWeb());
+						
+						modelClases.setRowCount(0);
+						List<Clase> clases = profesor.getArrayClases();
+						for(Clase c: clases) {
+							modelClases.addRow(new Object[] {c.getNombreClase(), c.getFechaClase().toString(), c.getHoraInicio()});
+						}
+
+						tablaClases.setVisible(true);
+						scrollPaneClases.setVisible(true);
+					}
+				} else {
+					nickname.setText("");
+					nombre.setText("");
+					apellido.setText("");
+					email.setText("");
+					fechaNac.setText("");
 					labelDescripcion.setVisible(false);
 					labelBiografia.setVisible(false);
 					labelSitioWeb.setVisible(false);
 					labelClases.setVisible(false);
 					tablaClases.setVisible(false);
 					scrollPaneClases.setVisible(false);
-					descripcion.setText("");
-					biografia.setText("");
-					sitioWeb.setText("");
-
-					modelRegistro.setRowCount(0);
-					List<Registro> registros = socio.getArrayRegistro();
-					for (Registro r: registros) {
-						modelRegistro.addRow(new Object[] {r.getclase().getNombreClase(), r.getfechaRegistro().toString()});
-					}
-
-					tablaRegistro.setVisible(true);
-					scrollPaneRegistros.setVisible(true);
-				} else {
-					Profesor profesor = (Profesor) usuario;
-					labelRegistros.setVisible(false);
-					labelDescripcion.setVisible(true);
-					labelBiografia.setVisible(true);
-					labelSitioWeb.setVisible(true);
-					labelClases.setVisible(true);
 					tablaRegistro.setVisible(false);
 					scrollPaneRegistros.setVisible(false);
-					descripcion.setText(profesor.getDescripcionGeneral());
-					biografia.setText(profesor.getBiografia());
-					sitioWeb.setText(profesor.getSitioWeb());
-					
-					modelClases.setRowCount(0);
-					List<Clase> clases = profesor.getArrayClases();
-					for(Clase c: clases) {
-						modelClases.addRow(new Object[] {c.getNombreClase(), c.getFechaClase().toString(), c.getHoraInicio()});
-					}
-
-					tablaClases.setVisible(true);
-					scrollPaneClases.setVisible(true);
+					labelRegistros.setVisible(false);
 				}
 			}
 		});
--- tarea1/src/main/java/presentacionModificacion/GUIModificarInstitucion.java
+++ tarea1/src/main/java/presentacionModificacion/GUIModificarInstitucion.java
@@ -4,7 +4,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
 
-public class GUIModificarActividadDescripcion extends JInternalFrame {
+public class GUIModificarInstitucion extends JInternalFrame {
 
 	/**
 	 * Launch the application.
@@ -13,7 +13,7 @@ public class GUIModificarActividadDescripcion extends JInternalFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					GUIModificarActividadDescripcion frame = new GUIModificarActividadDescripcion();
+					GUIModificarInstitucion frame = new GUIModificarInstitucion();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -25,7 +25,7 @@ public class GUIModificarActividadDescripcion extends JInternalFrame {
 	/**
 	 * Create the frame.
 	 */
-	public GUIModificarActividadDescripcion() {
+	public GUIModificarInstitucion() {
 		setBounds(100, 100, 450, 300);
 
 	}

eaa2a2012ef2c90f7df69ba866c1fd5f6fced6aa
cbusquets <busquetschristian30@gmail.com>
2023-09-03 00:55:52 -0300

    Se completa CU26 GUI ranking de dictado de clase.

--- tarea1/src/main/java/presentacionConsultas/GUIRankingActividadDeportiva.java
+++ tarea1/src/main/java/presentacionConsultas/GUIRankingActividadDeportiva.java
@@ -1,5 +1,7 @@
 package presentacionConsultas;
 
+import java.awt.EventQueue;
+
 import javax.swing.JButton;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
@@ -7,58 +9,34 @@ import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 import datatypes.DtActividad;
+import datatypes.DtClase;
 import interfaces.IActividadDeportiva;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
 import java.util.List;
 import java.awt.event.ActionEvent;
 
 
 
 public class GUIRankingActividadDeportiva extends JInternalFrame {
-	private JTable tablaRankingActividad = new JTable();
-	List<DtActividad> actividadesOrdenadas;
+	private JTable tablaRankingActividad;
 
-	private void setupActions(IActividadDeportiva iActividad) {
-		addComponentListener(new ComponentAdapter() {
-		@Override
-			public void componentShown(ComponentEvent e) {
-				DefaultTableModel tableModel = new DefaultTableModel();
-				tableModel.addColumn("Cantidad");
-				tableModel.addColumn("Nombre actividad");
-		        tableModel.addColumn("Costo");
-		        tableModel.addColumn("Descripción");
-		        
-		        JScrollPane scrollPane = new JScrollPane(tablaRankingActividad);
-		        scrollPane.setBounds(23, 108, 550, 312);
-		        getContentPane().add(scrollPane);
-		        
-		        tablaRankingActividad.setModel(tableModel);
-		        
-				System.out.println("se ejecuto");
-				actividadesOrdenadas = iActividad.getRankingActividades();
-				
-				if (actividadesOrdenadas.isEmpty()) {
-					
-					JOptionPane.showMessageDialog(null, "Error: No hay actividades para mostrar en el ranking.", "Error", JOptionPane.ERROR_MESSAGE);
-					
-				}else{	
-		         	 tableModel.setRowCount(0); // Limpiar las filas existentes	
-		         	   for (DtActividad ac : actividadesOrdenadas) {
-		         		   tableModel.addRow(new Object[]{ac.getCantidadDeClases() ,ac.getNombre(), ac.getCosto(), ac.getDescripcion()});
-				            
-		         	   	}
-				}
-			}
-		});
-	}
 	
 
 	public GUIRankingActividadDeportiva(IActividadDeportiva iActividad) {
-		setupActions(iActividad);
+		tablaRankingActividad = new JTable();
+		
+		DefaultTableModel tableModel = (DefaultTableModel) tablaRankingActividad.getModel();
+		tableModel.addColumn("Cantidad");
+		tableModel.addColumn("Nombre actividad");
+        tableModel.addColumn("Costo");
+        tableModel.addColumn("Descripción");
+        JScrollPane scrollPane = new JScrollPane(tablaRankingActividad);
+        scrollPane.setBounds(23, 108, 550, 312);
+        
+        getContentPane().add(scrollPane);
 		
 		JLabel lblRankingActividades = new JLabel("Ranking actividades");
 		lblRankingActividades.setBounds(217, 32, 164, 15);
@@ -67,7 +45,40 @@ public class GUIRankingActividadDeportiva extends JInternalFrame {
 		setClosable(true);
 		setTitle("Ranking Actividades ");
 		setBounds(100, 100, 610, 500);
-		getContentPane().setLayout(null);	
+		getContentPane().setLayout(null);
+		
+		JButton btnCargarRanking = new JButton("Cargar Ranking");
+		btnCargarRanking.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+						
+			List<DtActividad> actividadesOrdenadas = iActividad.getRankingActividades();
+					
+			if (actividadesOrdenadas.isEmpty()) {
+						
+				JOptionPane.showMessageDialog(null, "Error: No hay actividades para mostrar en el ranking.", "Error", JOptionPane.ERROR_MESSAGE);
+				
+			}else{	
+             	 tableModel.setRowCount(0); // Limpiar las filas existentes	
+             	   for (DtActividad ac : actividadesOrdenadas) {
+             		   tableModel.addRow(new Object[]{ac.getCantidadDeClases() ,ac.getNombre(), ac.getCosto(), ac.getDescripcion()});
+			            
+             	   	}
+				}	
+			}
+		});
+		
+		btnCargarRanking.setBounds(108, 431, 164, 25);
+		getContentPane().add(btnCargarRanking);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tableModel.setRowCount(0); // Limpiar las filas existentes
+				dispose();
+			}
+		});
+		btnCancelar.setBounds(323, 431, 164, 25);
+		getContentPane().add(btnCancelar);
 
 	}
 
--- tarea1/src/main/java/presentacionConsultas/GUIRankingDictadoClase.java
+++ tarea1/src/main/java/presentacionConsultas/GUIRankingDictadoClase.java
@@ -7,10 +7,17 @@ import javax.swing.JInternalFrame;
 import interfaces.IClase;
 import interfaces.IInstitucionDeportiva;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
+import datatypes.DtClase;
+
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.awt.event.ActionEvent;
 
 public class GUIRankingDictadoClase extends JInternalFrame {
 	private JTable tablaRankingClaseDictado;
@@ -20,31 +27,57 @@ public class GUIRankingDictadoClase extends JInternalFrame {
 		
 		setClosable(true);
 		setTitle("Ranking Dictado Clase");
-		setBounds(100, 100, 608, 500);
+		setBounds(100, 100, 610, 411);
 		getContentPane().setLayout(null);
-		
-		JButton btnCargarRanking = new JButton("Cargar Ranking");
-		btnCargarRanking.setBounds(108, 431, 164, 25);
-		getContentPane().add(btnCargarRanking);
-		
-		JButton btnCancelar = new JButton("Cancelar");
-		btnCancelar.setBounds(323, 431, 164, 25);
-		getContentPane().add(btnCancelar);
 		
 		tablaRankingClaseDictado = new JTable();
 		
 		DefaultTableModel tableModel = (DefaultTableModel) tablaRankingClaseDictado.getModel();
+		tableModel.addColumn("Cantidad");
         tableModel.addColumn("Nombre de Clase");
         tableModel.addColumn("Fecha de la clase");
         tableModel.addColumn("URL de Clase");
         JScrollPane scrollPane = new JScrollPane(tablaRankingClaseDictado);
-        scrollPane.setBounds(23, 270, 550, 150);
+        scrollPane.setBounds(23, 83, 550, 202);
         
         getContentPane().add(scrollPane);
 		
 		JLabel lblRankingDictadoClase = new JLabel("Ranking dictado clase");
-		lblRankingDictadoClase.setBounds(185, 33, 164, 15);
+		lblRankingDictadoClase.setBounds(210, 33, 164, 15);
 		getContentPane().add(lblRankingDictadoClase);
+		
+		JButton btnCargarRanking = new JButton("Cargar Ranking");
+		btnCargarRanking.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				List<DtClase> claseOrdenadas = iClase.getRankingClases();
+				
+				if (claseOrdenadas.isEmpty()) {
+							
+					JOptionPane.showMessageDialog(null, "Error: No hay clases para mostrar en el ranking.", "Error", JOptionPane.ERROR_MESSAGE);
+					
+				}else{	
+	             	 tableModel.setRowCount(0); // Limpiar las filas existentes	
+	             	   for (DtClase cl : claseOrdenadas) {
+	             		   tableModel.addRow(new Object[]{cl.getCantidadClases() ,cl.getNombre(), cl.getFechaClase(), cl.getUrl()});
+				            
+	             	   	}
+					}	
+				
+			}
+		});
+		btnCargarRanking.setBounds(124, 325, 164, 25);
+		getContentPane().add(btnCargarRanking);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tableModel.setRowCount(0); // Limpiar las filas existentes
+				dispose();
+			}
+		});
+		btnCancelar.setBounds(330, 325, 164, 25);
+		getContentPane().add(btnCancelar);
 
 	}
 }
